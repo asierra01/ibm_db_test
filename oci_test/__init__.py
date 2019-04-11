4677c4a074aa0fe8b04044e5542c7adb27dfbff9
asierra01 <asierra01@gmail.com>
2019-04-11 18:01:54 -0400

    Remove duplicated directory

--- oci_test/__init__.py
+++
@@ -1,6 +0,0 @@
-from __future__ import absolute_import
-
-__all__ = ['DB2_OCI_TEST', 'db2_oci_func', 'Db2OCITest_UnitTest']
-
-from .test_ibm_oci_test import DB2_OCI_TEST, Db2OCITest_UnitTest
-from .oci_function_list import db2_oci_func
