01344ffd5b3721bb315e94cd2e15e33c9ae71a34
asierra01 <asierra01@gmail.com>
2019-04-11 17:59:47 -0400

    renaming directory

---
+++ oci_test_cases/__init__.py
@@ -0,0 +1,6 @@
+from __future__ import absolute_import
+
+__all__ = ['Db2OCITest_UnitTest', 'run_oci_unittest']
+
+from .ibm_oci_test import Db2OCITest_UnitTest, run_oci_unittest
+#from .oci_function_list import db2_oci_func
